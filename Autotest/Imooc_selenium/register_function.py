9f56f56213b38c1dd6e6ee895a9030308234db5e
likangming <32701693+likangming@users.noreply.github.com>
2018-12-17 18:45:05 +0800

    Update register_function.py

--- Autotest/Imooc_selenium/register_function.py
+++ Autotest/Imooc_selenium/register_function.py
@@ -7,37 +7,41 @@ import random
 from PIL import Image
 from find_element import FindElement
 from ShowapiRequest import ShowapiRequest
+
+
 class RegisterFunction(object):
     def __init__(self,url,i):
         self.driver = self.get_driver(url,i)
-    #获取driver并且打开url
+        
+    # 获取driver并且打开url
     def get_driver(self,url,i):
         if i == 1:
             driver = webdriver.Chrome()
         elif i == 2:
-
             driver = webdriver.Firefox()
         else:
             driver = webdriver.Edge()
         driver.get(url)
         driver.maximize_window()
         return driver
-    #输入用户信息
+    
+    # 输入用户信息
     def send_user_info(self,key,data):
         self.get_user_element(key).send_keys(data)
     
-    #定位用户信息，获取element
+    # 定位用户信息，获取element
     def get_user_element(self,key):
         find_element = FindElement(self.driver)
         user_element = find_element.get_element(key)
         return user_element
 
-    #获取随机数
+    # 获取随机数
     def get_range_user(self):
         user_info = ''.join(random.sample('1234567890abcdefghijklmn',8))
         return user_info
 
-    #获取图片
+    
+    # 获取图片
     def get_code_image(self,file_name):
         self.driver.save_screenshot(file_name)
         code_element = self.get_user_element("code_image")
@@ -49,7 +53,8 @@ class RegisterFunction(object):
         img = im.crop((left,top,right,height))
         img.save(file_name)
 
-    #解析图片获取验证码
+        
+    # 解析图片获取验证码
     def code_online(self,file_name):
         self.get_code_image(file_name)
         r = ShowapiRequest("http://route.showapi.com/184-4","62626","d61950be50dc4dbd9969f741b8e730f5" )
@@ -61,7 +66,6 @@ class RegisterFunction(object):
         text = res.json()['showapi_res_body']['Result']
         return text
     
-
     def main(self):
         user_name_info = self.get_range_user()
         user_email = user_name_info+"@163.com"
@@ -84,3 +88,4 @@ if __name__ == '__main__':
     for i in range(3):
         register_function = RegisterFunction('http://www.5itest.cn/register',i)
         register_function.main()
+        
