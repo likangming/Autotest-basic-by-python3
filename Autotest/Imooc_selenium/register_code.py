e4f03345eaa643054122e21fba1d6fba707afd3f
likangming <32701693+likangming@users.noreply.github.com>
2018-12-17 18:43:31 +0800

    Update register_code.py

--- Autotest/Imooc_selenium/register_code.py
+++ Autotest/Imooc_selenium/register_code.py
@@ -4,24 +4,29 @@ import time
 import random
 from PIL import Image
 from util.ShowapiRequest import ShowapiRequest
+
+
 driver = webdriver.Chrome()
-#浏览器初始化
+# 浏览器初始化
 def driver_init(): 
     driver.get("http://www.5itest.cn/register")
     driver.maximize_window()
     time.sleep(5)
 
-#获取element信息
+    
+# 获取element信息
 def get_element(id):
     element = driver.find_element_by_id(id)
     return element
 
-#获取随机数
+
+# 获取随机数
 def get_range_user():
     user_info = ''.join(random.sample('1234567890abcdefghijklmn',8))
     return user_info
 
-#获取图片
+
+# 获取图片
 def get_code_image(file_name):
     driver.save_screenshot(file_name)
     code_element = driver.find_element_by_id("getcode_num")
@@ -33,7 +38,8 @@ def get_code_image(file_name):
     img = im.crop((left,top,right,height))
     img.save(file_name)
 
-#解析图片获取验证码
+    
+# 解析图片获取验证码
 def code_online(file_name):
     r = ShowapiRequest("http://route.showapi.com/184-4","62626","d61950be50dc4dbd9969f741b8e730f5" )
     r.addBodyPara("typeId", "35")
@@ -44,7 +50,8 @@ def code_online(file_name):
     text = res.json()['showapi_res_body']['Result']
     return text
 
-#运行主程序
+
+# 运行主程序
 def run_main():
     user_name_info = get_range_user()
     user_email = user_name_info+"@163.com"
@@ -59,7 +66,5 @@ def run_main():
     get_element("register-btn").click()
     driver.close()
 
+    
 run_main()
-
-
-
